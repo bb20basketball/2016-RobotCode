46475fab3db585877fb90901d2549e3d5c4c41c7
bb20basketball <bb20basketball@gmail.com>
2016-03-07 20:15:59 -0600

    Allowed it to chose between values accurately

--- The_Great_Vision_Experiment.py
+++ The_Great_Vision_Experiment.py
@@ -5,13 +5,12 @@ import networktables
 
 ip = "127.0.0.1"
 
-NetworkTable.setIPAddress(ip)
-NetworkTable.setClientMode()
-NetworkTable.initialize()
+"""NetworkTable.setIPAddress(ip)
+NetworkTable.setClientMode()   Sometimes you need Client mode, sometimes you don't
+NetworkTable.initialize()"""
 
 table = NetworkTable.getTable('/GRIP/myContoursReport')
 default = networktables.NumberArray()
-
 while True:
     try:
         table.retrieveValue('centerX', default)
@@ -20,7 +19,18 @@ while True:
     else:
         
         if len(default)>0:
-            vision_number=default[0]
+            if len(default)==1:
+                vision_number=default[0]
+            else:
+                good=default[0]
+                normal=abs(default[0]-160)
+                for i in default[1:]:
+                    total=abs(i-160)
+                    if total <= normal:
+                        normal=total
+                        good=i
+                vision_number=good
+                    
 
             #For safety reasons, you can press B and it will stop the auto line up
             if vision_number > 180:
@@ -33,4 +43,5 @@ while True:
                 print"to da left"
             else:
                 print "you good"
+            print vision_number
     time.sleep(1)
