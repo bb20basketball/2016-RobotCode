46b609fa0a36da274fa349599de1371fb5693a7b
bb20basketball <bb20basketball@gmail.com>
2016-07-21 22:46:01 -0500

    Updated with actual robot values

--- robot.py
+++ robot.py
@@ -85,14 +85,14 @@ class MyRobot(wpilib.IterativeRobot):
         self.vision_numberY=0
         self.automatedShooter = 0
         #PIDs!!
-        kP = 0.03
+        kP = 0.3
         kI = 0.00
         kD = 0.00
         kF = 0.00
         turnController = wpilib.PIDController(kP, kI, kD, kF, self.navx, output=self)
         turnController.setInputRange(-180.0,  180.0)
         turnController.setOutputRange(-.5, .5)
-        turnController.setAbsoluteTolerance(2.0)
+        turnController.setAbsoluteTolerance(1.0)
         turnController.setContinuous(True)
 
         self.turnController = turnController
@@ -107,7 +107,7 @@ class MyRobot(wpilib.IterativeRobot):
         self.auto_chooser.addObject("Reacher", "3")
         self.auto_chooser.addObject("Crosser RT", "4")
         wpilib.SmartDashboard.putData('Choice', self.auto_chooser)
-
+        self.auto_aline_autoNow=False
         self.shooter_counter=0
         #Shooter speed
         self.shooter_high=.47
@@ -133,7 +133,7 @@ class MyRobot(wpilib.IterativeRobot):
         self.auto_aline_auto=False
         self.auto_drive1=0
         self.auto_drive2=0
-        self.state=4
+        self.state=5
         self.arcade_drive.setSafetyEnabled(False)
         #Gets the choice
         self.final_choice=self.auto_chooser.getSelected()
@@ -324,7 +324,7 @@ class MyRobot(wpilib.IterativeRobot):
 
     def teleopInit(self):
         #starting out the state at neutral motors
-        self.state=4
+        self.state=5
         self.total_pan=0
         self.turn_state=2
         self.desired=0
@@ -380,7 +380,7 @@ class MyRobot(wpilib.IterativeRobot):
             rotation = (self.boost*(1*self.controller.getX()))#In theory it should be right
 
         try:
-            self.arcade_drive.arcadeDrive((self.boost*self.controller.getY()), rotation)
+            self.arcade_drive.arcadeDrive(rotation, (self.boost*self.controller.getY()))
         except:
             if not self.isFMSAttached():
                 raise
@@ -462,29 +462,32 @@ class MyRobot(wpilib.IterativeRobot):
 
         try:
             self.vision_table.retrieveValue('centerX', self.vision_x)
+            
             self.vision_table.retrieveValue('centerY', self.vision_y)
+            print (self.vision_x)
             #self.vision_x = [210] #Just for offline testing
             #self.vision_y = [150]
         except KeyError:
             self.turner=False
         else:
             if len(self.vision_x)>0 and self.auto_alineX.get() and self.vision_state == 3 or self.auto_aline_auto:
+                self.auto_aline_auto=False
                 self.degrees=self.gyroMagic(self.find_bestX())
                 self.vision_state = 1
             elif len(self.vision_x)>0 and self.auto_alineY.get() or self.auto_aline_autoY:
                 self.vision_numberY=self.vision_y[0]
-
-                if self.vision_numberY > 50:
+                
+                if self.vision_numberY > 72:
                     self.turner=True
                     self.ready_aline=False
-                    self.auto_calc=(((self.vision_numberY-50)/210)*.15)+.25
+                    self.auto_calc=(((self.vision_numberY-72)/210)*.15)+.2
                     self.auto_drive1=(-1*self.auto_calc)
                     self.auto_drive2=(-1*self.auto_calc)
 
-                elif self.vision_numberY < 20:
+                elif self.vision_numberY < 54:
                     self.turner=True
                     self.ready_aline=False
-                    self.auto_calc=(((20-self.vision_numberY)/20)*.15)+.25
+                    self.auto_calc=(((54-self.vision_numberY)/54)*.15)+.15
                     self.auto_drive1=(1*self.auto_calc)#May be backwards
                     self.auto_drive2=(1*self.auto_calc)
                 else:
@@ -504,9 +507,9 @@ class MyRobot(wpilib.IterativeRobot):
             self.vision_numberX=self.vision_x[0]
         else:
             good=self.vision_x[0]
-            normal=abs(self.vision_x[0]-160)
+            normal=abs(self.vision_x[0]-147)
             for i in self.vision_x[1:]:
-                total=abs(i-160)
+                total=abs(i-147)
                 if total <= normal:
                     normal=total
                     good=i
@@ -514,15 +517,16 @@ class MyRobot(wpilib.IterativeRobot):
         return self.vision_numberX
 
     def gyroMagic(self, x):
-        angle = ((x-110)*(60/320)) #Change the second part of this equation if it is not right
+        angle = ((x-170)*(60/320)) #Change the second part of this equation if it is not right
 
         yaw=self.navx.getYaw()
+        print (yaw)
         if yaw > 0:
-            self.desiredAngle=self.navx.getYaw()+(angle*(227/180)) #Times 227/180 because the gyro is offset
+            self.desiredAngle=self.navx.getYaw()+(angle*(180/180)) #Times 227/180 because the gyro is offset
             if self.desiredAngle>179:
                 self.desiredAngle -= 360
         else:
-            self.desiredAngle=self.navx.getYaw()+(angle*(133/180))
+            self.desiredAngle=self.navx.getYaw()+(angle*(180/180))
             if self.desiredAngle<-179:
                 self.desiredAngle += 360
 
@@ -539,7 +543,7 @@ class MyRobot(wpilib.IterativeRobot):
             if self.turnController.onTarget() or self.cancel.get():
                 self.vision_state=3
                 self.rotateReady=False
-                self.auto_aline_auto=False
+                self.auto_aline_autoNow=False
 
 
     def fire(self):
@@ -550,9 +554,10 @@ class MyRobot(wpilib.IterativeRobot):
             self.timer.reset()
             self.state=1
             self.ready=False
-            self.last = 3200
+            self.last = 4800
             self.speedShooter=0
             self.b = 0
+            self.counterAuto = 0
 
         elif self.state == 1:
             self.fire_counter=True
@@ -570,12 +575,14 @@ class MyRobot(wpilib.IterativeRobot):
 
         elif self.state==2:
             self.shooter_piston=2
-            self.difference = 3200 - self.encoder.getRate() #3200 is the target RPM
+            self.difference = 4800 - self.encoder.getRate() #3200 is the target RPM
             self.normalDiff = (self.difference)*.0000009 #Finds difference and averages then multiplies gain
             self.totalSpeed+=self.normalDiff #Adds it to the PWM speed
 
-            if not self.auto_aline_autoY:
+            if not self.auto_aline_autoY and self.automatedShooter != 0 and self.counterAuto==0:
                 self.auto_aline_auto=True
+                self.auto_aline_autoNow=True
+                self.counterAuto=1
 
             if self.totalSpeed > 1: self.totalSpeed=1
             elif self.totalSpeed < 0: self.totalSpeed=0
@@ -586,23 +593,32 @@ class MyRobot(wpilib.IterativeRobot):
             self.speedShooter=self.totalSpeed #Sets the speed
 
             print ("Encoder: "+str(self.encoder.getRate())+" ... Speed: "+str(self.speedShooter))
-            if self.encoder.getRate()<3300 and self.encoder.getRate()>3100: #Probably start with a bigger range at first
-                if not self.auto_aline_auto:
-                    self.speedShooter=self.totalSpeed
-                    self.shooter_piston=1
-                    self.state=3
-                    self.ready=True
+        
+            if self.encoder.getRate()<4800 and self.encoder.getRate()>4500:
+                self.state=3
+
+        elif self.state==3:
+            
+            #Probably start with a bigger range at first
+            if not self.auto_aline_autoNow:
+                self.speedShooter=self.totalSpeed
+                self.shooter_piston=1
+                self.state=4
+                self.ready=True
+            else:
+                print ("nope")
+                self.speedShooter=self.totalSpeed
 
 
-        elif self.state == 3:
+        elif self.state == 4:
             self.speedShooter=self.totalSpeed
             if self.timer.hasPeriodPassed(.75):
-                self.state=4
+                self.state=5
 
-        elif self.state==4 and self.intake_button.get():
+        elif self.state==5 and self.intake_button.get():
             self.intake()
 
-        elif self.state==4:
+        elif self.state==5:
             self.fire_counter=False
             self.speedShooter=0
             #self.shooter_piston=1 I could change this and see if that helps stop the immediate retract
